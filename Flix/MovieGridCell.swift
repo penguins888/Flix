79c9d07c99326396481fda703618b125aee01a88
penguins888 <57269064+penguins888@users.noreply.github.com>
2020-02-27 22:58:51 -0800

    Implementation of Flix part 2

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Michelle Lu on 2/26/20.
+//  Copyright © 2020 meep. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
